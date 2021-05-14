d48ad8fc870f7acd94e054bb0325111d89bb7635
eray_vscode <ibrahimoglu.eray@gmail.com>
2021-05-14 06:20:13 +0300

    scheduler

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <ucontext.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include<unistd.h>
 
 #define STACK_SIZE 4096
 #define empty 0
@@ -30,20 +32,37 @@ void initializeThread (int index, int count_val, ucontext_t *arg){
 }
 
 void createThread (ucontext_t *arg){
-    getcontext(&*arg);
+    getcontext(arg);
     arg->uc_link = &c0;
     arg->uc_stack.ss_sp = malloc(STACK_SIZE);
     arg->uc_stack.ss_size = STACK_SIZE;
-    makecontext(&*arg, (void (*)(void))decrement, 0);
+    makecontext(arg, (void (*)(void))decrement, 0);
+}
+
+void runThread (ucontext_t *arg){
+    swapcontext(&c0, arg);
+}
+
+void exitThread (ucontext_t *arg){
+    free(arg->uc_stack.ss_sp);
+}
+
+void PWF_scheduler (void){
+    printf("schedule");
 }
 
 int main()
 {   
+    signal(SIGALRM, PWF_scheduler);
+    alarm(2);
+    sleep(3);
     initializeThread(1, 5, &c1);
     createThread(&c1);
     getcontext(&c0);
     printf("%p, %p", c1.uc_link, &c0);
-    swapcontext(&c0, &c1);
+    runThread(&c1);
+    runThread(&c1);
+    runThread(&c1);
     printf("exiting");
     free(c1.uc_stack.ss_sp);
     return 0;
