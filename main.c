c281d15f02c493edc5b1466767c2183ed5bd94e0
eray_vscode <ibrahimoglu.eray@gmail.com>
2021-05-22 19:42:26 +0300

    formatting and shares

--- main.c
+++ main.c
@@ -10,12 +10,14 @@
 #define ready 1
 #define running 2
 #define finished 3
+#define SRTF 1
+#define PandWF 0
 
 ucontext_t context_array[6];
 int running_thread, running_thread_temp, randint, sum;
 int * current_value;
-int flag = 0, flag_firstRunScheduler = 1, flag_firstRunSelect = 1, finishedNum = 0, counter;
-int i;
+int flag = 0, flag_firstRunScheduler = 1, flag_firstRunSelect = 1, finishedNum = 0, counter, common_denominator, min, schedulerType;
+int i, j;
 int thresholds[5];
 
 struct ThreadInfo {
@@ -99,33 +101,68 @@ void printStatus (void){
 }
 
 int selectThread (void){
-    if(flag_firstRunSelect){
-        for (i = 1; i < 6; ++i){ 
+    
+    if(schedulerType == PandWF){
+        if(flag_firstRunSelect){
+            sum = 0;
+            for (i = 1; i < 6; ++i){ 
+                sum += thread_array[i].count_val;
+                thresholds[i-1] = sum;
+            }      
+            flag_firstRunSelect = 0;
+        }
+        randint = rand() % sum;
+        for (i = 1; i < 6; ++i){
+            if(randint < thresholds[i-1]){
+                if(thread_array[i].state != finished){
+                    running_thread = i;
+                    break;
+                }
+                else{
+                    selectThread();
+                    break;
+                }
+            }
+        }
+    }
+    else{
+        
+
+    }
+    
+}
+
+int printShares (void){
+    min = thread_array[1].count_val;
+    sum = 0;
+    for (i = 1; i < 6; ++i){ 
             sum += thread_array[i].count_val;
-            thresholds[i-1] = sum;
-            printf("%d\n", sum);
-        }
-        flag_firstRunSelect = 0;
+        }  
+    
+    for (i = 2; i < 6; ++i){ 
+        if(min > thread_array[i].count_val){ min = thread_array[i].count_val;}
     }
-    randint = rand() % sum;
-    for (i = 1; i < 6; ++i){
-        if(randint < thresholds[i-1]){
-            if(thread_array[i].state != finished){
-                running_thread = i;
-                break;
-            }
-            else{
-                selectThread();
-                break;
-            }
+
+    for (i = 1; i <= min ; ++i){ 
+        for(j = 1; j<6; ++j){
+            if(thread_array[j].count_val % i == 0 ){continue;}
+            else{break;}
         }
+        if(j == 6){ common_denominator = i;}
     }
+
+    printf("Share:\n");
+    for (i = 1; i < 6; ++i){ 
+        printf("%d/%d\t", thread_array[i].count_val/common_denominator, sum/common_denominator);
+    }
+    printf("\n\n");
 }
 
 void scheduler (void){
     if(flag_firstRunScheduler == 0){ running_thread_temp = running_thread; }
     else { flag_firstRunScheduler = 0;
-        printf("Threads:\nT1\tT2\tT3\tT4\tT5");
+        printShares();
+        printf("Threads:\nT1\tT2\tT3\tT4\tT5\n");
         }
     if(thread_array[running_thread].state == finished){finishedNum ++;}
     if(finishedNum != 5){ 
@@ -153,12 +190,34 @@ void SRTF_scheduler (void){
 }
 
 int main()
-{   
+{      
+    printf("Select P&WF(0) or SRTF(1): ");
+    scanf("%d", &schedulerType);
+    if(schedulerType != 0 && schedulerType != 1){
+        printf("Invalid input\n");
+        return 0;
+    }
+    printf("\n");
+   
+
+    int nArray[5];
+    printf("Enter 'n' values below for each thread (Even positive integers only):\n");    
+    for(i=0; i<5; i++){
+        printf("n%d: ", i+1);
+        scanf("%d", &nArray[i]);
+        if(nArray[i]%2 != 0 || nArray[i] < 2 ){
+            printf("Invalid Input\n");
+            return 0;
+        }
+    }
+    printf("\n");
+    
+
     srand(time(NULL));
     getcontext(&context_array[0]);
     i = 1;
     while(i < 6){
-        initializeThread(i, i*2, &context_array[i]);
+        initializeThread(i, nArray[i-1], &context_array[i]);
         createThread(&context_array[i], &thread_array[i]);
         i++;
     }
@@ -166,6 +225,5 @@ int main()
     while(finishedNum != 5){
         PWF_scheduler();
     }
-    printf("exiting");
     return 0;
 }
